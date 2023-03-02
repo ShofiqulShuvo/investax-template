23bb6f34238c1651a7d185a087709d9657b4dfd9
Talat Anik <talat_anik@hotmail.com>
2023-03-02 13:46:35 +0600

    swiper card issues fixed

--- js/my-swiper.js
+++ js/my-swiper.js
@@ -13,10 +13,27 @@ const swiper = new Swiper('.swiper', {
   //   prevEl: '.swiper-button-prev',
   // },
 
-  cssMode: true,
+  resistance: true,
+  effect: 'slide',
+  speed: 700,
+  preventInteractionOnTransition: true,
+  cssMode: false,
 
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
 })
+
+function scrollSlider (event) {
+  event.preventDefault()
+  if(event.deltaY >40) 
+    swiper.slideNext()
+  
+  if(event.deltaY <-50)
+    swiper.slidePrev()
+
+}
+
+const el = document.querySelector('body')
+el.onwheel = scrollSlider
